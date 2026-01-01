0db16def8bd83bc0400308537351bf465cba0ddc
Dave Brown <dfbrownnz@gmail.com>
2026-01-01 15:59:18 -0500

    add task description

--- Todo.cs
+++ Todo.cs
@@ -14,23 +14,25 @@ public enum TodoGroup
 public class Todo
 {
     [Required]
-    [Description("The unique name of the project is used to find a file name")]
+    [Description("The unique name of the project is used to find a file name with all the task")]
     public required string ProjectId { get; set; }
     [Required]
-    [Description("The unique id of the task ")]
+    [Description("The unique id of the task - leave blank to create a new task. ")]
     public required int Id { get; set; }
     [Required]
-    [Description("The unique name of the task ")]
+    [Description("The full Details for the task ")]
+    public required string Description { get; set; }
+    [Required]
+    [Description("The name of the task - a group name the user can set ")]
     public required string Name { get; set; }
     [Required]
-    [Description("The unique name of the task group Configuration Testing etc ")]
+    [Description("The group name - a group name the user cannot set ")]
     public required string Group { get; set; }
-
     [Required]
-    [Description("The Status of Complete Not Started etc  ")]
+    [Description("The Status Name eg Complete, Not Started, etc  ")]
     public required string StatusFlag { get; set; }
     [Required]
-    [Description("Date the task was changes in yyyyMMdd format")]
+    [Description("Date the task was changed in yyyyMMdd format")]
 
     public required string StatusDate { get; set; }
 
